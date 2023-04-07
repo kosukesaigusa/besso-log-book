de0da92d71ef38a0e815dede3ed9fc5364badbd0
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-04-07 14:31:20 +0900

    feat: add VisitoLogCreateController

---
+++ lib/visitor_log/ui/visitor_log_create_controller.dart
@@ -0,0 +1,6 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final visitLogCreateController =
+    Provider.autoDispose((_) => VisitLogCreateController());
+
+class VisitLogCreateController {}
