d8cde921ed82ff8e8398316e0923e64a6e660099
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-04-15 14:02:56 +0900

    feat: add visitor logs UI base

--- lib/visitor_log/ui/visitor_logs.dart
+++ lib/visitor_log/ui/visitor_logs.dart
@@ -2,7 +2,15 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../firestore/firestore_models/visitor_log.dart';
+import '../../firestore/firestore_repository/visitor_log.dart';
 import '../../router/router.gr.dart';
+
+final visitorLogsStreamProvider =
+    StreamProvider.autoDispose<List<VisitorLog>>((ref) {
+  final repository = ref.watch(visitorLogRepositoryProvider);
+  return repository.subscribeVisitorLogs();
+});
 
 @RoutePage()
 class VisitorLogsPage extends ConsumerWidget {
@@ -12,6 +20,11 @@ class VisitorLogsPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // return ref.watch(visitorLogsStreamProvider).when(
+    //       data: (visitorLogs) => const SizedBox(),
+    //       loading: () => const SizedBox(),
+    //       error: (_, __) => const SizedBox(),
+    //     );
     return Scaffold(
       appBar: AppBar(title: const Text('訪問記録一覧')),
       body: Center(
